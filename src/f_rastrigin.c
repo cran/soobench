b96d074c6513c5dee57f1726516ec03b6654f346
Olaf Mersmann <olafm@p-value.net>
2012-03-01 00:00:00 +0000

    version 1.0-71

---
+++ src/f_rastrigin.c
@@ -0,0 +1,12 @@
+#include "soobench.h"
+
+const double f_rastrigin(const double *x, const size_t n) {
+    double res = 0.0;
+    size_t i;
+    
+    for (i = 0; i < n; ++i) {
+        const double xi = x[i];
+        res += xi * xi - 10 * cos( 2 * M_PI * xi) + 10;
+    }
+    return res;
+}
---
+++ src/f_sphere.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+const double f_sphere(const double *x, const size_t n) {
+    size_t i;
+    double res = 0.0;
+    for (i = 0; i < n; ++i) {
+        res += x[i] * x[i];
+    }
+    return res;
+}
